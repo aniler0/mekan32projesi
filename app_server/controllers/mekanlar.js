1ac0edc626cb697794d765916f5d905de255104e
aniler0 <anil.er@hotmail.com>
2020-11-27 20:50:19 +0200

    Yeni eklenen kısımlar

---
+++ app_server/controllers/mekanlar.js
@@ -0,0 +1,17 @@
+const anaSayfa=function(req, res, next) {
+  res.render('mekanlar-liste', { title: 'Anasayfa' });
+}
+
+const mekanBilgisi=function(req, res, next) {
+  res.render('mekan-detay', { title: 'Mekan Bilgisi'});
+}
+
+const yorumEkle=function(req, res, next) {
+  res.render('yorum-ekle', { title: 'Yorum Ekle' });
+}
+
+module.exports={
+anaSayfa,
+mekanBilgisi,
+yorumEkle
+}
