1838f3ef0eb4bda22b9c6141059620885896bbde
aniler0 <anil.er@hotmail.com>
2020-12-15 23:52:52 +0200

    yeni değişiklikler

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
+require('./app_server/models/db');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname,'app_server','views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -23,12 +24,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,108 +1,109 @@
 var express = require('express');
 var router = express.Router();
-const anaSayfa=function(req, res, next) {
+const anaSayfa = function (req, res, next) {
   res.render('mekanlar-liste',
-   { 
-     'baslik': 'Anasayfa',
-     'sayfaBaslik':{
-       'siteAd':'Mekan32',
-       'aciklama':'Isparta civarındaki mekanları keşfedin'
-     },
-     'footer':'Anıl Er 2020',
-     'mekanlar':[
-       {
-         'ad':'Starbucks',
-         'adres':'Iyaşpark AVM',
-         'puan':'3',
-         'imkanlar':['kahve','çay','pasta'],
-         'mesafe':'9km'
-       },
-       {
-         'ad':'Gloria Jeans',
-         'adres':'Iyaşpark AVM',
-         'puan':'3',
-         'imkanlar':['kahve','kek','pasta'],
-         'mesafe':'9km'
-         
-       },
-       {
-         'ad':'Köfteci Yusuf',
-         'adres':'Iyaş Bulvar AVM',
-         'puan':'4',
-         'imkanlar':['köfte','kahvaltı'],
-         'mesafe':'11km'
-       },
-       {
-         'ad':'Alaçatı Muhallebicisi',
-         'adres':'Iyaş AVM',
-         'puan':'2',
-         'imkanlar':['kahve','çay','pasta','tatlı'],
-         'mesafe':'8km'
-       },
-       {
-         'ad':'Casia Lounge',
-         'adres':'Modernevler Mahallesi,Hüseyin Avni Paşa Caddesi',
-         'puan':'3',
-         'imkanlar':['kahve','çay','pasta'],
-         'mesafe':'7km'
-       }
-       
-     ]
-   }
-   );
+    {
+      'baslik': 'Anasayfa',
+      'sayfaBaslik': {
+        'siteAd': 'Mekan32',
+        'aciklama': 'Isparta civarındaki mekanları keşfedin'
+      },
+
+      'footer': 'Anıl Er 2020',
+      'mekanlar': [
+        {
+          'ad': 'Starbucks',
+          'adres': 'Iyaşpark AVM',
+          'puan': '3',
+          'imkanlar': ['kahve', 'çay', 'pasta'],
+          'mesafe': '9km'
+        },
+        {
+          'ad': 'Gloria Jeans',
+          'adres': 'Iyaşpark AVM',
+          'puan': '3',
+          'imkanlar': ['kahve', 'kek', 'pasta'],
+          'mesafe': '9km'
+
+        },
+        {
+          'ad': 'Köfteci Yusuf',
+          'adres': 'Iyaş Bulvar AVM',
+          'puan': '4',
+          'imkanlar': ['köfte', 'kahvaltı'],
+          'mesafe': '11km'
+        },
+        {
+          'ad': 'Alaçatı Muhallebicisi',
+          'adres': 'Iyaş AVM',
+          'puan': '2',
+          'imkanlar': ['kahve', 'çay', 'pasta', 'tatlı'],
+          'mesafe': '8km'
+        },
+        {
+          'ad': 'Casia Lounge',
+          'adres': 'Modernevler Mahallesi,Hüseyin Avni Paşa Caddesi',
+          'puan': '3',
+          'imkanlar': ['kahve', 'çay', 'pasta'],
+          'mesafe': '7km'
+        }
+
+      ]
+    }
+  );
 }
 
-const mekanBilgisi=function(req, res) {
-  res.render('mekan-detay', { 
+const mekanBilgisi = function (req, res) {
+  res.render('mekan-detay', {
     'baslik': 'Mekan Bilgisi',
-    'sayfaBaslik':'Starbucks',
-    'footer':'Anıl Er 2020',
-    'mekanBilgisi':{
-      'ad':'Starbucks',
-      'adres':'Iyaşpark AVM',
-      'puan':3,
-      'imkanlar':['Kahve','Pasta','Kek'],
-      'koordinatlar':{
-        'enlem':37.78253988714279,
-        'boylam':30.544915909008672
+    'sayfaBaslik': 'Starbucks',
+    'footer': 'Anıl Er 2020',
+    'mekanBilgisi': {
+      'ad': 'Starbucks',
+      'adres': 'Iyaşpark AVM',
+      'puan': 3,
+      'imkanlar': ['Kahve', 'Pasta', 'Kek'],
+      'koordinatlar': {
+        'enlem': 37.78253988714279,
+        'boylam': 30.544915909008672
       },
-      'saatler':[
+      'saatler': [
         {
-          'gunler':'Pazartesi-Cuma',
-          'acilis':'7:00',
-          'kapanis':'23:00',
-          'kapali':false
+          'gunler': 'Pazartesi-Cuma',
+          'acilis': '7:00',
+          'kapanis': '23:00',
+          'kapali': false
         },
         {
-          'gunler':'Cumartesi',
-          'acilis':'9:00',
-          'kapanis':'22:00',
-          'kapali':false
+          'gunler': 'Cumartesi',
+          'acilis': '9:00',
+          'kapanis': '22:00',
+          'kapali': false
         },
         {
-          'gunler':'Pazar',
-          'kapali':true
+          'gunler': 'Pazar',
+          'kapali': true
         }
       ],
-      'yorumlar':[
+      'yorumlar': [
         {
-          'yorumYapan':'Anıl Er',
-          'puan':3,
-          'tarih':'01.12.2020',
-          'yorumMetni':'Kahveleri güzel beğeniyorum.'
+          'yorumYapan': 'Anıl Er',
+          'puan': 3,
+          'tarih': '01.12.2020',
+          'yorumMetni': 'Kahveleri güzel beğeniyorum.'
         }
       ]
     }
-    
+
   });
 }
 
-const yorumEkle=function(req, res, next) {
+const yorumEkle = function (req, res, next) {
   res.render('yorum-ekle', { title: 'Yorum Ekle' });
 }
 
-module.exports={
-anaSayfa,
-mekanBilgisi,
-yorumEkle
+module.exports = {
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle
 }
