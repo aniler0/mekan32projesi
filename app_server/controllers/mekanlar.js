462cd96fa327e964474bcd48479101b6a736e3c0
aniler0 <anil.er@hotmail.com>
2021-01-16 12:14:21 +0200

    Hata düzeltmesi

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,6 +1,6 @@
 var request = require('postman-request');
 var apiSecenekleri = {
-  sunucu: "http://localhost:3000",
+  sunucu: "https://aniler1821012180.herokuapp.com",
   apiYolu: '/api/mekanlar/'
 }
 var istekSecenekleri
